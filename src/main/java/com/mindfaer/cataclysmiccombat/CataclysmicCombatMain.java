b4a68d5a193c6867abecf45e5f34c69d3fef76b8
Ash Sylvan <75921990+MindFaer@users.noreply.github.com>
2023-06-24 22:27:06 -0400

    Initial Commit

---
+++ src/main/java/com/mindfaer/cataclysmiccombat/CataclysmicCombatMain.java
@@ -0,0 +1,45 @@
+package com.mindfaer.cataclysmiccombat;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(CataclysmicCombatMain.MODID)
+public class CataclysmicCombatMain
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "cataclysmiccombat";
+    // Directly reference a slf4j logger
+    public CataclysmicCombatMain()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+
+    {
+   }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+       }
+    }
+}
